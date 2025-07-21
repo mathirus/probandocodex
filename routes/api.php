14ca50e95a9dc5ac8a33d307a0599c39a6e1acf9
mathirus <44822178+mathirus@users.noreply.github.com>
2025-07-21 19:18:35 -0300

    Simplify auth using DNI header

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\RankingController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('email.auth')->group(function () {
+Route::middleware('dni.auth')->group(function () {
     Route::get('exams', [ExamController::class, 'index']);
     Route::get('exams/{exam}/questions', [ExamController::class, 'questions']);
     Route::post('exams/{exam}/submit', [ExamController::class, 'submit']);
